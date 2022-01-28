656697148398fe175db427d1d28a36c2ac993e7b
Ayan Shrivastava <ayanshrivastava2020@gmail.com>
2022-01-28 15:26:58 +0530

    feat: add feature options

---
+++ src/Components/Header.jsx
@@ -0,0 +1,20 @@
+import {Link} from 'react-router-dom'
+import Logo from '../images/trf_logo.png';
+import Menu from '../images/Menu.png';
+function Header() {
+    return (  
+        <div>
+            <div className="header">
+                <img src={Logo} className='logoImg' />
+                <div className="navBar">
+                    <h4>menu</h4>
+                    <img src={Menu} alt="" />
+                </div>
+            </div>
+                
+        
+        </div>
+    );
+}
+
+export default Header;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,65 @@
 // import {link} from '../routes';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import Header from '../Components/Header';
+import PresenceImg from '../images/presence-gif.png';
 function Home() {
-    return (  
+    return (
         <div>
-            
+            <Header />
+            <section className="hero">
+                <div className="heroTitle">
+                    <h6>We'll do it for you!</h6>
+                    <div className='heroHeading'>
+                        <h1>Website </h1> <h1>Portfolio </h1><h1>Branding</h1>
+                    </div>
+                </div>
+                <h3 className='heroSubTitle'>The Royal Flush designs and develops products for you.
+                    We build portfolio websites, web
+                    applications, and elaborate online business services.</h3>
+
+            </section>
+            <section className="featured">
+                {/* <div className="title featureTitle">
+                    <h3>Featured</h3>
+                    <h3>Creation</h3>
+                </div> */}
+                <div className="featuredContent">
+                    <div className="featuredItem">
+                        <div className="featureInfo">
+                            <p>Presence</p>
+                            <h2>Web Application For Attendance</h2>
+                            <h4>web application, design, product,
+                                college</h4>
+                        </div>
+                        <div className="featureImg">
+                            <img src={PresenceImg} alt="Presence" />
+                        </div>
+                    </div>
+                    <div className="featuredItem">
+                        <div className="featureInfo">
+                            <p>Presence</p>
+                            <h2>Web Application For Attendance</h2>
+                            <h4>web application, design, product,
+                                college</h4>
+                        </div>
+                        <div className="featureImg">
+                            <img src={PresenceImg} alt="Presence" />
+                        </div>
+                    </div>
+                    <div className="featuredItem">
+                        <div className="featureInfo">
+                            <p>Presence</p>
+                            <h2>Web Application For Attendance</h2>
+                            <h4>web application, design, product,
+                                college</h4>
+                        </div>
+                        <div className="featureImg">
+                            <img src={PresenceImg} alt="Presence" />
+                        </div>
+                    </div>
+                </div>
+                <button className='btn-products'>View all projects</button>
+            </section>
         </div>
     );
 }
