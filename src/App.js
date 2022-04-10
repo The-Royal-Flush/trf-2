12b28e482912969108e06b77396552df257494aa
AYAN SHRIVASTAVA <ayanshrivastava2020@gmail.com>
2022-04-10 11:22:42 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import {BrowserRouter as Router,Routes , Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
-
+import About from './Components/About';
 import Home from './Components/Home';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element ={<Home />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
       </Routes>
     </Router>
   );
